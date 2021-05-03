36cedefbf7f9e053c34b5c3e6088a0ae54f9a179
Alex Standiford <a@alexstandiford.com>
2021-05-03 16:24:12 -0700

    Fixes issues where extended plugins did not get registered items.

--- scripts.php
+++ scripts.php
@@ -4,13 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $class ) {
-	if ( 'Underpin\Underpin' === $class ) {
+add_action( 'underpin/before_setup', function ( $instance ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Script.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Scripts.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Script_Instance.php' );
-		Underpin\underpin()->loaders()->add( 'scripts', [
+		$instance->loaders()->add( 'scripts', [
 			'registry' => 'Underpin_Scripts\Loaders\Scripts',
 		] );
-	}
 } );
