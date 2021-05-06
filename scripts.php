b2fbe6328e6174ab77ae9fcf5145af2033721db6
Alex Standiford <a@alexstandiford.com>
2021-05-05 17:17:04 -0700

    Corrects underpin autoloader

--- scripts.php
+++ scripts.php
@@ -4,11 +4,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 // Add this loader.
-add_action( 'underpin/before_setup', function ( $file ) {
+add_action( 'underpin/before_setup', function ( $file, $class ) {
 		require_once( plugin_dir_path( __FILE__ ) . 'Script.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Scripts.php' );
 		require_once( plugin_dir_path( __FILE__ ) . 'Script_Instance.php' );
-		Underpin\underpin()->get( $file )->loaders()->add( 'scripts', [
+		Underpin\underpin()->get( $file, $class )->loaders()->add( 'scripts', [
 			'registry' => 'Underpin_Scripts\Loaders\Scripts',
 		] );
-}, 5 );
+}, 5, 2 );
