02f266d0066439e84cccf447ae4ae8a2b038aadb
Alex Standiford <a@alexstandiford.com>
2021-06-21 20:45:57 -0600

    Updates script loader to use middleware. #1

--- scripts.php
+++ scripts.php
@@ -5,10 +5,12 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 // Add this loader.
 add_action( 'underpin/before_setup', function ( $file, $class ) {
-		require_once( plugin_dir_path( __FILE__ ) . 'Script.php' );
-		require_once( plugin_dir_path( __FILE__ ) . 'Scripts.php' );
-		require_once( plugin_dir_path( __FILE__ ) . 'Script_Instance.php' );
-		Underpin\underpin()->get( $file, $class )->loaders()->add( 'scripts', [
-			'registry' => 'Underpin_Scripts\Loaders\Scripts',
-		] );
+	require_once( plugin_dir_path( __FILE__ ) . 'Script.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'Scripts.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'Script_Instance.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'Enqueue_Admin_Script.php' );
+	require_once( plugin_dir_path( __FILE__ ) . 'Enqueue_Script.php' );
+	Underpin\underpin()->get( $file, $class )->loaders()->add( 'scripts', [
+		'registry' => 'Underpin_Scripts\Loaders\Scripts',
+	] );
 }, 5, 2 );
