c94b0f7fbe959ef98dce9489bce3b0364496960e
Alex Standiford <a@alexstandiford.com>
2022-01-06 07:42:27 -0600

    Fixes a bug that prevented enqueue script conditional from working.

--- lib/Factories/Enqueue_Script_Conditional.php
+++ lib/Factories/Enqueue_Script_Conditional.php
@@ -27,27 +27,12 @@ class Enqueue_Script_Conditional extends Enqueue_Conditional {
 		parent::__construct( $args );
 	}
 
-	public function enqueue() {
-		// Only enqueue if this is the block editor.
-		if ( $this->should_enqueue() ) {
-			if ( $this->loader_item instanceof Script ) {
-				$this->loader_item->enqueue();
-			} else {
-				Logger::log( 'warning', 'rest_middleware_action_failed_to_run', 'Middleware action failed to run. Rest_Middleware expects to run on a Script loader.', [
-					'loader'  => get_class( $this->loader_item ),
-					'expects' => 'Underpin\Scripts\Abstracts\Script',
-				] );
-			}
-		}
-	}
-
 	protected function should_enqueue() {
 		return $this->set_callable( $this->should_enqueue_callback, $this->loader_item );
 	}
 
 	public function update( $instance, Storage $args ) {
 		parent::update( $instance, $args );
-		$this->loader_item = $instance;
 		add_action( 'wp_enqueue_scripts', [ $this, 'enqueue' ] );
 	}
 
